e0584e8d661b2250c8493ae4740eb5f8b2caa329
Andrew D. Robertson <andrew.robertson@sussex.ac.uk>
2015-10-14 13:04:27 +0100

    clustering package

---
+++ src/main/java/uk/ac/susx/tag/classificationframework/clusters/ClusteredProcessedInstance.java
@@ -0,0 +1,25 @@
+package uk.ac.susx.tag.classificationframework.clusters;
+
+import uk.ac.susx.tag.classificationframework.datastructures.ProcessedInstance;
+
+/**
+ * Represents a ProcessedInstance that has been clustered in some fashion.
+ * User: Andrew D. Robertson
+ * Date: 14/10/2015
+ * Time: 12:59
+ */
+public class ClusteredProcessedInstance {
+    
+    private ProcessedInstance document;
+    private double[] clusterVector;
+
+    public ClusteredProcessedInstance(ProcessedInstance document, double[] clusterVector) {
+        this.document = document;
+        this.clusterVector = clusterVector;
+    }
+
+    public ProcessedInstance getDocument() {  return document; }
+    public void setDocument(ProcessedInstance document) { this.document = document;}
+    public double[] getClusterVector() { return clusterVector; }
+    public void setClusterVector(double[] clusterVector) { this.clusterVector = clusterVector; }
+}

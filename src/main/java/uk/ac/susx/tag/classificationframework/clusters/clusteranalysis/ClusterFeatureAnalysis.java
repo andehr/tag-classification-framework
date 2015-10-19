b634d397d68e1c60b3890f7cde156e8d48091cf5
Andrew D. Robertson <andrew.robertson@sussex.ac.uk>
2015-10-19 16:13:46 +0100

    clustering counts

--- src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/ClusterFeatureAnalysis.java
+++ src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/ClusterFeatureAnalysis.java
@@ -16,8 +16,20 @@ public class ClusterFeatureAnalysis {
     private Int2IntOpenHashMap featureCounts;
     private Int2IntOpenHashMap[] jointCounts;
 
-    public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents, ClusterMembershipFunction f) {
-        int numClusters = documents.iterator().next().getClusterVector().length;
+    private int[] totalFeatureCountPerCluster;
+
+    private FeatureLabelJointCounter counts;
+
+    public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents){
+        this(documents, FeatureLabelJointCounter.createDocumentBased(documents, new FeatureLabelJointCounter.HighestProbabilityOnly()));
+    }
+
+    public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents, FeatureLabelJointCounter c) {
+        counts = c;
+
+        numClusters = documents.iterator().next().getClusterVector().length;
+        totalFeatureCount = 0;
+        totalFeatureCountPerCluster = new int[numClusters];
 
         // Initialise the counting data structures
         jointCounts = new Int2IntOpenHashMap[numClusters];
@@ -36,8 +48,44 @@ public class ClusterFeatureAnalysis {
         }
     }
 
-    public interface ClusterMembershipFunction {
-        boolean isDocumentInCluster(ClusteredProcessedInstance instance, int clusterIndex);
+    public List<String> getTopFeatures(int clusterIndex, int K, FeatureExtractionPipeline pipeline){
+        return getTopFeatures(clusterIndex, K).stream()
+                .map(pipeline::labelString)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getTopFeatures(int clusterIndex, int K) {
+        return new LikelihoodPriorRatioOrdering(clusterIndex).greatestOf(jointCounts[clusterIndex].keySet(), K);
+    }
+
+//    private double featurePrior(int feature){
+//        return featureCounts.get(feature) / totalFeatureCount;
+//    }
+//
+//    private double likelihoodFeatureGivenCluster(int feature, int cluster){
+//        return jointCounts[cluster].get(feature) / totalFeatureCountPerCluster[cluster];
+//    }
+
+    public class LikelihoodPriorRatioOrdering extends Ordering<Integer> {
+
+        int clusterIndex = 0;
+
+        public LikelihoodPriorRatioOrdering(int clusterIndex) {
+            this.clusterIndex = clusterIndex;
+        }
+
+        public void setClusterIndex(int clusterIndex) {
+            this.clusterIndex = clusterIndex;
+        }
+
+        @Override
+        public int compare(Integer feature1, Integer feature2) {
+
+            double leftRatio = Math.log(counts.featurePrior(feature1)) - Math.log(counts.likelihoodFeatureGivenLabel(feature1, clusterIndex));
+            double rightRatio = Math.log(counts.featurePrior(feature2)) - Math.log(counts.likelihoodFeatureGivenLabel(feature2, clusterIndex));
+
+            return Double.compare(leftRatio, rightRatio);
+        }
     }
 
 }

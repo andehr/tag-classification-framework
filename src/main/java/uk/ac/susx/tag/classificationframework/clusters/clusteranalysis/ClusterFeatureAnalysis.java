244fb3b6b28ebe50d3a8fdd5c803a0c9794d6351
Andrew D. Robertson <andrew.robertson@sussex.ac.uk>
2015-10-20 13:40:03 +0100

    counts done

--- src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/ClusterFeatureAnalysis.java
+++ src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/ClusterFeatureAnalysis.java
@@ -1,9 +1,13 @@
 package uk.ac.susx.tag.classificationframework.clusters.clusteranalysis;
 
+import com.google.common.collect.Ordering;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import uk.ac.susx.tag.classificationframework.clusters.ClusteredProcessedInstance;
+import uk.ac.susx.tag.classificationframework.featureextraction.pipelines.FeatureExtractionPipeline;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,39 +17,15 @@ import java.util.Collection;
  */
 public class ClusterFeatureAnalysis {
 
-    private Int2IntOpenHashMap featureCounts;
-    private Int2IntOpenHashMap[] jointCounts;
-
-    private int[] totalFeatureCountPerCluster;
-
-    private FeatureLabelJointCounter counts;
+    private FeatureClusterJointCounter counts;
 
     public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents){
-        this(documents, FeatureLabelJointCounter.createDocumentBased(documents, new FeatureLabelJointCounter.HighestProbabilityOnly()));
+        this(documents, new FeatureClusterJointCounter.FeatureBasedCounts(), new FeatureClusterJointCounter.HighestProbabilityOnly());
     }
 
-    public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents, FeatureLabelJointCounter c) {
+    public ClusterFeatureAnalysis(Collection<ClusteredProcessedInstance> documents, FeatureClusterJointCounter c, FeatureClusterJointCounter.ClusterMembershipTest t) {
         counts = c;
-
-        numClusters = documents.iterator().next().getClusterVector().length;
-        totalFeatureCount = 0;
-        totalFeatureCountPerCluster = new int[numClusters];
-
-        // Initialise the counting data structures
-        jointCounts = new Int2IntOpenHashMap[numClusters];
-        for (int i = 0; i < jointCounts.length; i++)
-            jointCounts[i] = new Int2IntOpenHashMap();
-
-        featureCounts = new Int2IntOpenHashMap();
-
-        // Obtain feature counts, and joint counts of features per cluster
-        for (ClusteredProcessedInstance instance : documents) {
-            for (int clusterIndex=0; clusterIndex < numClusters; clusterIndex++){
-                for (int feature : instance.getDocument().features){
-
-                }
-            }
-        }
+        c.count(documents, t);
     }
 
     public List<String> getTopFeatures(int clusterIndex, int K, FeatureExtractionPipeline pipeline){
@@ -55,34 +35,21 @@ public class ClusterFeatureAnalysis {
     }
 
     public List<Integer> getTopFeatures(int clusterIndex, int K) {
-        return new LikelihoodPriorRatioOrdering(clusterIndex).greatestOf(jointCounts[clusterIndex].keySet(), K);
+        return new LikelihoodPriorRatioOrdering(clusterIndex).greatestOf(counts.getFeaturesInCluster(clusterIndex), K);
     }
-
-//    private double featurePrior(int feature){
-//        return featureCounts.get(feature) / totalFeatureCount;
-//    }
-//
-//    private double likelihoodFeatureGivenCluster(int feature, int cluster){
-//        return jointCounts[cluster].get(feature) / totalFeatureCountPerCluster[cluster];
-//    }
 
     public class LikelihoodPriorRatioOrdering extends Ordering<Integer> {
 
         int clusterIndex = 0;
-
         public LikelihoodPriorRatioOrdering(int clusterIndex) {
-            this.clusterIndex = clusterIndex;
-        }
-
-        public void setClusterIndex(int clusterIndex) {
             this.clusterIndex = clusterIndex;
         }
 
         @Override
         public int compare(Integer feature1, Integer feature2) {
 
-            double leftRatio = Math.log(counts.featurePrior(feature1)) - Math.log(counts.likelihoodFeatureGivenLabel(feature1, clusterIndex));
-            double rightRatio = Math.log(counts.featurePrior(feature2)) - Math.log(counts.likelihoodFeatureGivenLabel(feature2, clusterIndex));
+            double leftRatio = Math.log(counts.featurePrior(feature1)) - Math.log(counts.likelihoodFeatureGivenCluster(feature1, clusterIndex));
+            double rightRatio = Math.log(counts.featurePrior(feature2)) - Math.log(counts.likelihoodFeatureGivenCluster(feature2, clusterIndex));
 
             return Double.compare(leftRatio, rightRatio);
         }
---
+++ src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/FeatureClusterJointCounter.java
@@ -0,0 +1,225 @@
+package uk.ac.susx.tag.classificationframework.clusters.clusteranalysis;
+
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import uk.ac.susx.tag.classificationframework.clusters.ClusteredProcessedInstance;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew D. Robertson
+ * Date: 19/10/2015
+ * Time: 12:11
+ */
+public abstract class FeatureClusterJointCounter {
+
+    public abstract void count(Collection<ClusteredProcessedInstance> documents, ClusterMembershipTest t);
+
+    public abstract double featurePrior(int feature);
+
+    public abstract double likelihoodFeatureGivenCluster(int feature, int cluster);
+
+    public abstract IntSet getFeatures();
+
+    public abstract IntSet getFeaturesInCluster(int clusterIndex);
+
+    public static class DocumentBasedCounts extends FeatureClusterJointCounter {
+
+        public int numDocuments;
+        public int[] numDocumentsPerCluster;
+        public Int2IntOpenHashMap featureCounts;
+        public Int2IntOpenHashMap[] jointCounts;
+
+        @Override
+        public void count(Collection<ClusteredProcessedInstance> documents, ClusterMembershipTest t) {
+            // Initialise the counting data structures
+            int numClusters = documents.iterator().next().getClusterVector().length;
+
+            numDocumentsPerCluster = new int[numClusters];
+            featureCounts = new Int2IntOpenHashMap();
+            jointCounts = new Int2IntOpenHashMap[numClusters];
+            for (int i = 0; i < jointCounts.length; i++)
+                jointCounts[i] = new Int2IntOpenHashMap();
+
+            // Obtain feature counts, and joint counts of features per cluster
+            numDocuments = documents.size();
+
+            for (ClusteredProcessedInstance instance : documents) {
+
+                IntSet features = new IntOpenHashSet(instance.getDocument().features);
+
+                for (int feature : features)
+                    featureCounts.addTo(feature, 1);
+
+                for (int clusterIndex=0; clusterIndex < numClusters; clusterIndex++){
+                    if (t.isDocumentInCluster(instance, clusterIndex)){
+                        numDocumentsPerCluster[clusterIndex]++;
+                        for (int feature : features) {
+                            jointCounts[clusterIndex].addTo(feature, 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public double featurePrior(int feature) {
+            return featureCounts.get(feature) / numDocuments;
+        }
+
+        @Override
+        public double likelihoodFeatureGivenCluster(int feature, int cluster) {
+            return jointCounts[cluster].get(feature) / numDocumentsPerCluster[cluster];
+        }
+
+        @Override
+        public IntSet getFeatures() {
+            return featureCounts.keySet();
+        }
+
+        @Override
+        public IntSet getFeaturesInCluster(int clusterIndex) {
+            return jointCounts[clusterIndex].keySet();
+        }
+
+
+    }
+
+    public static class FeatureBasedCounts extends FeatureClusterJointCounter {
+
+        public int totalFeatureCount;
+        public Int2IntOpenHashMap featureCounts;
+        public Int2IntOpenHashMap[] jointCounts;
+        private int[] totalFeatureCountPerCluster;
+
+        @Override
+        public void count(Collection<ClusteredProcessedInstance> documents, ClusterMembershipTest t) {
+            // Initialise the counting data structures
+            int numClusters = documents.iterator().next().getClusterVector().length;
+
+            totalFeatureCount = 0;
+            totalFeatureCountPerCluster = new int[numClusters];
+            featureCounts = new Int2IntOpenHashMap();
+
+            jointCounts = new Int2IntOpenHashMap[numClusters];
+            for (int i = 0; i < jointCounts.length; i++)
+                jointCounts[i] = new Int2IntOpenHashMap();
+
+
+            // Obtain feature counts, and joint counts of features per cluster
+            for (ClusteredProcessedInstance instance : documents) {
+
+                int[] features = instance.getDocument().features;
+
+                totalFeatureCount += features.length;
+
+                for (int feature : features)
+                    featureCounts.addTo(feature, 1);
+
+                for (int clusterIndex=0; clusterIndex < numClusters; clusterIndex++){
+                    if (t.isDocumentInCluster(instance, clusterIndex)){
+                        totalFeatureCountPerCluster[clusterIndex] += features.length;
+                        for (int feature : features) {
+                            jointCounts[clusterIndex].addTo(feature, 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public double featurePrior(int feature) {
+            return featureCounts.get(feature) / totalFeatureCount;
+        }
+
+        @Override
+        public double likelihoodFeatureGivenCluster(int feature, int cluster) {
+            return jointCounts[cluster].get(feature) / totalFeatureCountPerCluster[cluster];
+        }
+
+        @Override
+        public IntSet getFeatures() {
+            return featureCounts.keySet();
+        }
+
+        @Override
+        public IntSet getFeaturesInCluster(int clusterIndex) {
+            return jointCounts[clusterIndex].keySet();
+        }
+    }
+
+    /**
+     * Cluster membership testing.
+     *
+     * In order to produce the counts of how many times a feature occurs within a particular cluster,
+     * we must be able to decide whether or not a document is in a cluster.
+     *
+     * A class implementing the ClusterMembershipTest gets to decide whether a document is considered
+     * part of a cluster.
+     *
+     * For example, the HighestProbabilityOnly implementation allows the document to only be part of the
+     * cluster with which it has the highest probability of membership (clusterVector being treated as
+     * vector of membership probabilities).
+     */
+    public interface ClusterMembershipTest {
+
+        /**
+         * Called once per document. Perform any setup required before the cluster membership
+         * testing happens for each cluster.
+         */
+        void setup(ClusteredProcessedInstance instance);
+
+        /**
+         * Called N times per document, where N is the number of clusters.
+         * Must return true if the document is considered to belong to the specified cluster.
+         */
+        boolean isDocumentInCluster(ClusteredProcessedInstance instance, int clusterIndex);
+    }
+
+    /**
+     * Allows the document to only be part of the cluster with which it has the highest
+     * probability of membership (clusterVector being treated as vector of membership probabilities).
+     */
+    public static class HighestProbabilityOnly implements ClusterMembershipTest{
+        private int highestClusterIndex = 0;
+
+        public void setup(ClusteredProcessedInstance instance){
+            double[] clusterVector = instance.getClusterVector();
+            highestClusterIndex = 0;
+            double currentMax = clusterVector[0];
+            for (int i = 0; i < clusterVector.length; i++) {
+                if (clusterVector[i] > currentMax) {
+                    highestClusterIndex = i;
+                    currentMax = clusterVector[i];
+                }
+            }
+        }
+
+        public boolean isDocumentInCluster(ClusteredProcessedInstance instance, int clusterIndex) {
+            return clusterIndex == highestClusterIndex;
+        }
+    }
+
+    /**
+     * Allows the document to be part of any cluster for which the document's membership
+     * probability is equal to or higher than some threshold.
+     */
+    public static class ProbabilityAboveThreshold implements ClusterMembershipTest{
+
+        private final double threshold;
+
+        public ProbabilityAboveThreshold(double threshold){
+            this.threshold = threshold;
+        }
+
+        public void setup(ClusteredProcessedInstance instance){
+            // None required
+        }
+
+        public boolean isDocumentInCluster(ClusteredProcessedInstance instance, int clusterIndex) {
+            return instance.getClusterVector()[clusterIndex] >= threshold;
+        }
+    }
+}

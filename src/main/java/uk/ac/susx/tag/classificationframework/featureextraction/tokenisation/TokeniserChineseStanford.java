e7def62861397ce7007e2abb7b58c0ce7184b949
Punchwes <pengqiwei.pqw@gmail.com>
2020-03-21 10:09:34 +0000

    add stanford chinese tokeniser

---
+++ src/main/java/uk/ac/susx/tag/classificationframework/featureextraction/tokenisation/TokeniserChineseStanford.java
@@ -0,0 +1,69 @@
+package uk.ac.susx.tag.classificationframework.featureextraction.tokenisation;
+
+import uk.ac.susx.tag.classificationframework.Util;
+import uk.ac.susx.tag.classificationframework.datastructures.AnnotatedToken;
+import uk.ac.susx.tag.classificationframework.datastructures.Document;
+import uk.ac.susx.tag.classificationframework.datastructures.Instance;
+
+
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.util.StringUtils;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.*;
+
+
+
+
+public class TokeniserChineseStanford implements Tokeniser {
+
+    private static final long serialVersionUID = 0L;
+    private transient StanfordCoreNLP pipeline;
+
+    public TokeniserChineseStanford() throws IOException {
+        loadPipeline();
+    }
+
+    @Override
+    public Document tokenise(Instance document) {
+
+        Document tokenised = new Document(document);
+        if (!Util.isNullOrEmptyText(document)) {
+            int end = 0;
+
+            Annotation annotation = new Annotation(document.text);
+            pipeline.annotate(annotation);
+            List<CoreLabel> tokens = annotation.get(CoreAnnotations.TokensAnnotation.class);
+            for (CoreLabel token : tokens) {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                int start = document.text.indexOf(word, end);
+                end = start + word.length();
+                AnnotatedToken annotatedToken = new AnnotatedToken(word);
+                annotatedToken.start(start);
+                annotatedToken.end(end);
+                tokenised.add(annotatedToken);
+            }
+        }
+
+        return tokenised;
+    }
+
+    @Override
+    public String configuration() {
+        return "";
+    }
+
+    public void loadPipeline(){
+        Properties props = StringUtils.argsToProperties("-props", "StanfordCoreNLP-chinese.properties");
+        props.setProperty("annotators", "tokenize");
+        pipeline = new StanfordCoreNLP(props);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        loadPipeline();
+    }
+}

5aea4a0cb762d7ae6863778106889f198c97c4cc
Chris Inskip <chrisji@hotmail.co.uk>
2020-12-07 11:49:48 +0000

    reorder cleaning to avoid emptystring

--- src/main/java/uk/ac/susx/tag/classificationframework/featureextraction/tokenisation/TokeniserChineseStanford.java
+++ src/main/java/uk/ac/susx/tag/classificationframework/featureextraction/tokenisation/TokeniserChineseStanford.java
@@ -16,8 +16,6 @@ import java.io.ObjectInputStream;
 import java.util.*;
 
 
-
-
 public class TokeniserChineseStanford implements Tokeniser {
 
     private static final long serialVersionUID = 0L;
@@ -31,16 +29,12 @@ public class TokeniserChineseStanford implements Tokeniser {
     public Document tokenise(Instance document) {
 
         Document tokenised = new Document(document);
+
+        // Remove potential chars that would break the pipeline
+        document.text = cleanInstanceText(document);
+
         if (!Util.isNullOrEmptyText(document)) {
             int end = 0;
-
-            // need to remove potential chars that would break the pipeline (mainly no-break space)
-            // want to replace cuz we still want to keep space in the text (first replaceAll statement)
-            document.text = document.text.replaceAll("[\\s\\p{Z}]", " ").trim();
-            // white-list to maintain, in order to be careful about other potential strange characters.
-            document.text = document.text.replaceAll("[^\\p{L}\\p{N}\\p{Z}\\p{Sm}\\p{Sc}\\p{Sk}\\p{P}\\p{Mc}]","");
-            // need to handle unexpected surrogate characters.
-            document.text = document.text.replaceAll("[^\u0000-\uffff]", "");
 
             Annotation annotation = new Annotation(document.text);
             pipeline.annotate(annotation);
@@ -57,6 +51,27 @@ public class TokeniserChineseStanford implements Tokeniser {
         }
 
         return tokenised;
+    }
+
+    /**
+     * Remove potential chars that would break the pipeline (mainly no-break space)
+     *
+     * @param instance
+     * @return
+     */
+    private static String cleanInstanceText(Instance instance) {
+        if (!Util.isNullOrEmptyText(instance)) {
+            // Replace all whitespace with space character
+            instance.text = instance.text.replaceAll("[\\s\\p{Z}]", " ").trim();
+
+            // white-list to maintain, in order to be careful about other potential strange characters.
+            instance.text = instance.text.replaceAll("[^\\p{L}\\p{N}\\p{Z}\\p{Sm}\\p{Sc}\\p{Sk}\\p{P}\\p{Mc}]", "");
+
+            // need to handle unexpected surrogate characters.
+            instance.text = instance.text.replaceAll("[^\u0000-\uffff]", "");
+        }
+
+        return instance.text;
     }
 
     @Override

e141da5a763b28adaeeaf892fb70c0eb8020a85f
Simon Wibberley <simon.wibberley@gmail.com>
2021-09-01 19:39:37 +0100

    refactored background count generation process, added german stop words, and german background routine

---
+++ src/main/java/uk/ac/susx/tag/classificationframework/clusters/clusteranalysis/BackgroundUtils.java
@@ -0,0 +1,105 @@
+package uk.ac.susx.tag.classificationframework.clusters.clusteranalysis;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import uk.ac.susx.tag.classificationframework.datastructures.Instance;
+import uk.ac.susx.tag.classificationframework.featureextraction.pipelines.FeatureExtractionPipeline;
+import uk.ac.susx.tag.classificationframework.featureextraction.pipelines.PipelineBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+public class BackgroundUtils {
+
+
+
+
+    public static Iterator<Instance> wikiIterator(File file) throws IOException {
+        Gson gson = new Gson();
+
+        LineIterator itr = FileUtils.lineIterator(file, "UTF-8");
+
+        return new Iterator<Instance>() {
+
+            int i = 0;
+
+            private Instance next = null;
+
+            @Override
+            public boolean hasNext() {
+                String index = null ;
+                String data = null ;
+                if(itr.hasNext()) {
+                    index = itr.nextLine();
+                }
+                if(itr.hasNext()) {
+                    data = itr.nextLine();
+                }
+
+                if(index == null || data == null) {
+                    return false;
+                } else {
+                    String text = (String) gson.fromJson(data, Map.class).get("text");
+                    String id = (String) gson.fromJson(index, Map.class).get("_id");
+                    next = new Instance(null, text, id);
+                    return true;
+                }
+            }
+
+            @Override
+            public Instance next() {
+                if(++i % 1000 == 0) {
+                    System.out.println(i);
+                }
+                return next;
+            }
+        };
+    }
+
+
+    public static void german() throws IOException {
+
+        String input = "/home/sw206/Downloads/dewiki-20210823-cirrussearch-content.json";
+        String pipelineOut = "/home/sw206/de-pipeline.ser";
+        String countsOut = "/home/sw206/de-wiki-20210823-cirrussearch-content-feat-counts.ser";
+
+        FeatureExtractionPipeline pipeline = new PipelineBuilder().build(new PipelineBuilder.OptionList() // Instantiate the pipeline.
+                .add("tokeniser", ImmutableMap.of(
+                                "type", "germanstanford",
+                                "filter_punctuation", true,
+                                "normalise_urls", true
+                        )
+                )
+                .add("remove_stopwords", ImmutableMap.of(
+                        "use", "true",
+                        "lang", "de"))
+//                .add("filter_regex", "[\\-（()）【\\[\\]】]")
+                .add("unigrams", true)
+        );
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(pipelineOut)))){
+            out.writeObject(pipeline);
+        }
+
+        File file = new File(input);
+
+        Iterator<Instance> itr = wikiIterator(file);
+
+        IncrementalFeatureCounter cNew = new IncrementalFeatureCounter(0.1);
+        cNew.incrementCounts(itr, pipeline, 500);
+        cNew.pruneFeaturesWithCountLessThanN(3);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(countsOut))){
+            out.writeObject(cNew);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        german();
+    }
+}
--- src/main/java/uk/ac/susx/tag/classificationframework/featureextraction/tokenisation/TokeniserGermanStanford.java
+++ src/main/java/uk/ac/susx/tag/classificationframework/featureextraction/tokenisation/TokeniserGermanStanford.java
@@ -1,26 +1,28 @@
 package uk.ac.susx.tag.classificationframework.featureextraction.tokenisation;
 
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.StringUtils;
 import uk.ac.susx.tag.classificationframework.Util;
 import uk.ac.susx.tag.classificationframework.datastructures.AnnotatedToken;
 import uk.ac.susx.tag.classificationframework.datastructures.Document;
 import uk.ac.susx.tag.classificationframework.datastructures.Instance;
 
-
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.util.StringUtils;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.*;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Properties;
 
-public class TokeniserArabicStanford implements Tokeniser {
+public class TokeniserGermanStanford implements Tokeniser {
     private static final long serialVersionUID = 0L;
     private transient StanfordCoreNLP pipeline;
 
-    public TokeniserArabicStanford() {
+    public TokeniserGermanStanford() {
         loadPipeline();
     }
 
@@ -32,17 +34,7 @@ public class TokeniserArabicStanford implements Tokeniser {
         if (!Util.isNullOrEmptyText(document)) {
             int end = 0;
 
-            // need to handle unexpected surrogate characters.
-            /* Ahmed Younes:
-            The previous comment was the original comment
-            My addition to this is that I am not sure if Arabic has similar issues as chinese but there are some issues i faced before regarding this
-            such as RTL code point \u200f some times it comes up when you tokenise it comes attached to the word or the character
-            also some times when you read Arabic file the beginning  of that file starts with \ufeff which is called the byte order mark BOM
-            i have decided to leave this line of code as i understand it is a range and i think the range contains both \u200f and
-            \ufeff but you might want to confirm that for me or remove it if it is extra */
-            /*Ahmed Younes: very important note it turns out that the affects the stop words removal some stop words include special characters which
-            * are removed during tokenisation and removing these characters prevent the removal of stop words */
-            Annotation annotation = new Annotation(document.text.replaceAll("[^\u0000-\uffff]", ""));
+            Annotation annotation = new Annotation(document.text);
             pipeline.annotate(annotation);
             // The characters get changed in this line after calling the previous method List<CoreLabel> words = (List)annotation.get(CoreAnnotations.TokensAnnotation.class);
             List<CoreLabel> tokens = annotation.get(CoreAnnotations.TokensAnnotation.class);
@@ -67,7 +59,7 @@ public class TokeniserArabicStanford implements Tokeniser {
 
     public void loadPipeline(){
         // here is that code that adds the Arabic properties and enable the stanford Arabic Tokeniser
-        Properties props = StringUtils.argsToProperties("-props", "StanfordCoreNLP-arabic.properties");
+        Properties props = StringUtils.argsToProperties("-props", "StanfordCoreNLP-german.properties");
         props.setProperty("annotators", "tokenize");
         pipeline = new StanfordCoreNLP(props);
     }
@@ -75,5 +67,16 @@ public class TokeniserArabicStanford implements Tokeniser {
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         loadPipeline();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        Tokeniser t = new TokeniserGermanStanford();
+
+        Instance instance = new Instance(null,"The brown fox jumped over the sleeping dog.", null);
+
+        System.out.println(t.tokenise(instance));
+
     }
 }
